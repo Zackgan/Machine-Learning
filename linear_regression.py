262ba6b8f6c4fdd812e60b1586ffa47c8f36c9e2
Zaki Gandara <you@example.com>
2019-08-01 23:02:53 +0700

    file piton contoh regresi linear

---
+++ linear_regression.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
+
+data = pd.read_csv("student-mat.csv", sep=";")
+
+print(data.head())
+
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]  # this is an attribute
+
+predict = "G3"  # this is labels
+
+# labels are what we want to get based on attributes
+# we can predict more labels
+
+X = np.array(data.drop([predict], 1))  # this is gonna return to us a new dataframe that just doesn't have G3 in it
+# (this up here is training data) #based on this training data we're gonna predict another value
+
+y = np.array(data[predict])  # this is going to be the labels
+
+x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+
+# essentially we're taking all of our attributes & labels and we're gonna split them up into four arrays
+# x_train is gonna be a section of this "x" and y_train is gonna be a section of this "y"
+
+linear = linear_model.LinearRegression()
+
+linear.fit(x_train, y_train)  # to find the best linear line (persamaan garis linear (y = mx + b))
+
+acc = linear.score(x_test, y_test)  # this is going to return us a value that is going to represent the accuracy of our
+# model
+
+print(acc)  # print the accuracy
+
+print("Co: \n", linear.coef_)  # kode print variabel x dalam persamaan garis linear
+print("intercept: \n", linear.intercept_)  # kode print variabel b dalam persamaan garis linear
+
+
+predictions = linear.predict(x_test)
+
+for x in range(len(predictions)):
+    print(predictions[x], x_test[x], y_test[x])
